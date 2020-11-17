97d7a5d41aa8ad893a149092e863d943e1b6b471
3dpmarkr <round.markl@gmail.com>
2020-11-17 17:56:50 +0000

    PID defaults HE and BED
    Improved default PID values for Hotend and Bed - based on PLA (205 Hotend / 60 Bed)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -499,9 +499,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  14.72 // 3dpmarkr
-    #define DEFAULT_Ki   0.89 // 3dpmarkr
-    #define DEFAULT_Kd 61.22 // 3dpmarkr
+    #define DEFAULT_Kp  23.25 // 3dpmarkr - original value 14.72
+    #define DEFAULT_Ki   1.98 // 3dpmarkr - original value 0.89
+    #define DEFAULT_Kd 68.33 // 3dpmarkr - original value 61.22
   #endif
 #endif // PIDTEMP
 
@@ -540,9 +540,9 @@
 
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  #define DEFAULT_bedKp 110.86		// 3dpmarkr - original value 10.00
+  #define DEFAULT_bedKi 9.46 		// 3dpmarkr - original value .023
+  #define DEFAULT_bedKd 866.37 		// 3dpmarkr - original value 305.4
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
